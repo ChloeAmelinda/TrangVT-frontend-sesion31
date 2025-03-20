09b8c39874b72aa48373618b7251ddc07ac613c8
ChloeAmelinda <trangthichngunuong@gmail.com>
2025-03-20 11:46:04 +0700

    Add files via upload

--- bai6.js
+++ bai6.js
@@ -1,10 +1,10 @@
 let name = "huanrose@gmail.com";
-const password = "123456";
+let password = "123456";
 
 document.getElementById("loginButton").onclick = function () {
 
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
 
 
     if (username === name && password === password) {
