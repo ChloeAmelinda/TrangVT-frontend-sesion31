ebbebd39941fdfcc128318dfc0af11fa63ee8511
ChloeAmelinda <trangthichngunuong@gmail.com>
2025-03-20 11:42:01 +0700

    Add files via upload

--- bai5.js
+++ bai5.js
@@ -12,19 +12,7 @@ const employees=[
     }
 ]
 
-// function hienthi(){
-//     let tablebody = document.getElementById("employeeTable")[0]
-//     tablebody.innerHTML =""
-//     for(let i =0; i<employees.length; i++){
-//         let employee =employees[i]
-//         let row = tablebody.insertRow()
-//         row.insertCell(0).innerText = employee.id
-//         row.insertCell(1).innerText = employee.name
-//         row.insertCell(2).innerText = employee.age
-//         row.insertCell(3).innerText = employee.address
-//     }
-// }
-// hienthi()
+
 
     function hienthi() {
             let tableBody = document.getElementById("employeeTable").getElementsByTagName("tbody")[0]; 
